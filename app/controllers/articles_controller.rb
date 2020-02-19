e963e6b13a961fbc0584db6864a7b17cca2cca0b
Moutasem Manaseer <mo3manaseer@gmail.com>
2020-02-19 15:11:27 +0200

    destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,12 @@ class ArticlesController < ApplicationController
 	def index
 					@articles = Article.all	
 	end
+	def destroy
+					@article = Article.find(params[:id])
+					@article.destroy
+					flash[:notice] = "Article was deleted"
+					redirect_to articles_path
+	end
 	def edit
 					@article=Article.find(params[:id])
 	end
